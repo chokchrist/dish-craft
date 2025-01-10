db69f3e0496e5a7491db695a19b9bec9ea0d618e
Christ J. <christ.jmanzano@gmail.com>
2025-01-10 14:30:51 +0000

    Minor changes

--- src/components/Claude Recipe/ClaudeRecipe.jsx
+++ src/components/Claude Recipe/ClaudeRecipe.jsx
@@ -4,7 +4,7 @@ import styles from "./ClaudeRecipe.module.scss";
 
 export default function ClaudeRecipe(props) {
   return (
-    <section className={styles["suggested-recipe-container"]} aria-live="polite">
+    <section className={styles["suggested-recipe-container"]} aria-live="polite" ref={props.ref}>
       <h2>Dish Craft recommends:</h2>
       <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
