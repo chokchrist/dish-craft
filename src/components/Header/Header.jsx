918c1ff89b99463b9fc118b7f2a1ff430b4a7103
Christ J. <christ.jmanzano@gmail.com>
2025-01-10 14:31:15 +0000

    Change import logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import DishCraftIcon from "/public/logo.svg";
+import LogoIcon from "/public/logo.svg";
 import styles from "./Header.module.scss";
 
 export default function Header() {
   return (
     <header>
-      <img src={DishCraftIcon} alt="Dish Craft Logo" className={styles.icon} />
+      <img src={LogoIcon} alt="Dish Craft Logo" className={styles.icon} />
       <h1 className={styles.title}>Dish Craft</h1>
     </header>
   );
