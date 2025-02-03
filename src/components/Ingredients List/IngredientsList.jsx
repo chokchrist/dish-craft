438b3cc97a0c931132b959931a592c127099ded2
Christ J. <christ.jmanzano@gmail.com>
2025-02-03 16:09:12 +0000

    Added loading state

--- src/components/Ingredients List/IngredientsList.jsx
+++ src/components/Ingredients List/IngredientsList.jsx
@@ -2,13 +2,12 @@
 import styles from "./IngredientsList.module.scss";
 
 export default function IngredientsList(props) {
-
   const ingredientsListItems = props.ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
 
   return (
-    <section>
+    <section ref={props.getRecipeSectionRef}>
       <h2>Ingredients on hand:</h2>
       <ul className={styles["ingredients-list"]} aria-live="polite">
         {ingredientsListItems}
@@ -19,7 +18,9 @@ export default function IngredientsList(props) {
             <h3>Ready for a recipe?</h3>
             <p>Generate a recipe from your list of ingredients.</p>
           </div>
-          <button onClick={props.getRecipe}>Get a recipe</button>
+          <button onClick={props.getRecipe}>
+            {props.loading ? "Loading recipe..." : "Get recipe"}
+          </button>
         </div>
       )}
     </section>
